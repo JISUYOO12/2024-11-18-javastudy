cb8edaaea4ddeb178154da0a69a0ea28f1fdbe17
sist-104 <sist-104@DESKTOP-UJN1E3D>
2024-11-18 14:18:09 +0900

    1111

---
+++ FirstProject/src/첫번째자바.java
@@ -0,0 +1,81 @@
+/*
+ 	1.대문자 구분
+ 	2. 들여쓰기
+ 	3.명령문이 종료 반드시 ;
+ 	 ; => 파이썬 /kotlin / javascript(;을 사용하지 않음)
+ 	4. 자바는 {}
+ 	5. 문법
+ 	   데이터저장 ===> 데이터 처리(요청) ===> 화면 출력 -명령프롬프트 : application
+ 	   													문법
+ 	   											브라우저 출력 : web application
+ 	   													주력
+ 	   											모바일 출력 : mobile application
+ 	   											spring : java / kotlin
+ 	   											  
+ 	   				연산자 / 제어문
+					============
+					메소드
+ 	   한개 저장(메모리) : 변수/상수
+ 	   여러개 저장 : 배열
+ 	   			  클라스
+ 	   			  
+ 	   =============
+ 	   프로그램 종료시에 사라진다
+ 	   파일 / 오라클  => 영구적인 저장
+ 	   
+ 	   ========> 기본문법
+ 	   1. 기능을 모아서 관리 
+ 	      ===
+ 	      => 단락 : 메소드
+ 	      => 클래스 / 객체 => 객체지향 프로그램
+ 	      => 프로그램 유지 (예외처리)
+ 	      =======================================================8장
+ 	      => 라이브러리 : 자바에 지원
+ 	      => 1차 응용프로그램
+ 	      =============================> web
+ 	      1. html : 브라우저에서 실행
+ 	      			자바는 브라우저에서는 일반 텍스트
+ 	      	=> 투박 => css / 자바스크립트
+ 	     2. JSP (요청 => HTML => Java)
+ 	     		응답 => java => html
+ 	     		
+ 	     	MVC
+ 	     	====
+ 	     	Front / Back
+ 	     	|         |
+ 	     
+ 	       JS/TS     자바/오라클/Spring
+ 	       =========================================+ Full Stack
+ 	    3. 통합 : Framwork : Spring
+ 	    		 기본 틀 => 메인보드
+
+ 		4. 데이터 분석 : Python / Numpy / Pandas / MatplaoLib
+ 		5. NoSql ==> elasticsearch : 오라클 역활
+ 		6. 배포 : AWS
+ 		===================================================
+ 		Spting-Boot / JPA / MYSQL / Reat-Query / Redux / Vue3 / NextJS / TypeScript / NodeJS
+ 		
+ 		자바 / 오라클 / 알고리즘
+ 		HTML / CSS / JavaScipr / JSP / Spring-Boot / Python / 데이터 분석 / elasticsearch
+ 		======================================================================================
+ 		Spring + MYBatis + JSP + JavaScript(Jquery)
+ 
+ 
+ */
+public class 첫번째자바 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		System.out.println("유지수");
+		{
+			
+			
+			
+			
+			
+			
+		}
+	}
+
+}
