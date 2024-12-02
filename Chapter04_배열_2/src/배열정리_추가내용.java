b65fa8b0ba496677034326bf1114bf1ff31f11a5
sist-104 <sist-104@DESKTOP-UJN1E3D>
2024-12-02 17:51:57 +0900

    1202

--- Chapter04_배열_2/src/배열정리_추가내용.java
+++ Chapter04_배열_2/src/배열정리_추가내용.java
@@ -15,7 +15,9 @@ public class 배열정리_추가내용 {
 		int[] arr_copy=arr.clone(); // 깊은복사 값을 통째로 복사해서 새로운 배열을 생성함
 		System.out.println(Arrays.toString(arr));
 		System.out.println(Arrays.toString(arr_copy));
-		//int[] arr_copy=arr; // 얕은복사 같은 메모리에 저장 => 아바타 => adapter
+		//int[] arr_copy=arr; // 얕은복사 같은 메모리에 저장 => 아바타 => proptotype => proxy 패턴
+		// 디자인 패턴 => 싱글턴 , factory ... 스프링에서는 8개의 패턴을 사용함
+		// GOF => 23개 패턴
 				
 		System.out.println("arr="+arr);
 		//주소값이 같으면 같은 데이터를 제어
