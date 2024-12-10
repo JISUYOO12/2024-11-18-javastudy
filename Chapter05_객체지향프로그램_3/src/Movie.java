9af74841612378afc9d7febfabe29c07f8dc0b97
sist-104 <sist-104@DESKTOP-UJN1E3D>
2024-12-10 17:54:43 +0900

    1210

---
+++ Chapter05_객체지향프로그램_3/src/Movie.java
@@ -0,0 +1,11 @@
+// 사용자 정의 데이터형 => 1938
+public class Movie {
+	String no; // 고유번호
+	String title;
+	String poster;
+	String actor;
+	String genre;
+	String regdate;
+	String grade;
+	String director;
+}
