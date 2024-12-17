1d4075e45aebce812f41da302e6e73e5a181d3b9
sist-104 <sist-104@DESKTOP-UJN1E3D>
2024-12-17 13:58:44 +0900

    1217

---
+++ Chapter05_정리/src/객체지향_정리.java
@@ -0,0 +1,14 @@
+//118~121 122(객체생성) => new 생성자
+//123page => 메모리 저장
+//125 => 참조
+//127=>접근범위
+//152 => getter / sestter
+//178
+public class 객체지향_정리 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
