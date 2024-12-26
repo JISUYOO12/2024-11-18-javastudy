1f9c0b1539f70dcfaeb16b69a4bb58aa275c5aec
sist-104 <sist-104@DESKTOP-UJN1E3D>
2024-12-26 17:06:27 +0900

    1226

---
+++ Chapter11_IO라이브러리_1/src/com/sist/io/Container.java
@@ -0,0 +1,7 @@
+package com.sist.io;
+import java.io.*;
+import com.sist.model.*;
+import java.util.*;
+public class Container {
+
+}
---
+++ Chapter11_IO라이브러리_1/src/com/sist/model/A.java
@@ -0,0 +1,6 @@
+package com.sist.model;
+import com.sist.io.I;
+
+//public class A implement I {
+
+//}
