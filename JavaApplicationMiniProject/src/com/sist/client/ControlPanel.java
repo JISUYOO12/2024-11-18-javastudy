91906406476d5b33a3b7622a7bcec90e61f4a161
sist-104 <sist-104@DESKTOP-UJN1E3D>
2025-01-17 17:44:16 +0900

    0117

---
+++ JavaApplicationMiniProject/src/com/sist/client/ControlPanel.java
@@ -0,0 +1,32 @@
+package com.sist.client;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.*;
+// 화면 변경 
+public class ControlPanel extends JPanel{
+    HomePenal hp;
+    ChatPenal cp;
+    FoodGenrePenal fgp;
+    FoodFindPenal ffp;
+    FoodDetailPenal fdp;
+    CardLayout card=new CardLayout();
+    public ControlPanel()
+    {
+    	setLayout(card);
+    	hp=new HomePenal(this);
+    	add("HOME",hp);
+    	cp=new ChatPenal(this);
+    	add("CHAT",cp);
+    	fgp=new FoodGenrePenal(this);
+    	add("FOOD",fgp);
+    	ffp=new FoodFindPenal(this);
+    	add("FIND",ffp);
+    	fdp=new FoodDetailPenal(this);
+    	add("DETAIL",fdp);
+    }
+    
+}
